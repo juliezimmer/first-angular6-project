22cd689822296df1317dd4d988b7f315d238c37f
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-05-13 14:25:14 -0500

    added a simple app that accepts output and displays it in the browser.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// This is the definition of the component
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// 'title' is seen in app.component.html as "Welcome to {{ title }}!" 
+// if title is changed to 'my App', the title on the browser page is changed to "Welcome to my App!"
+// This dynamically changes what browser output.
 export class AppComponent {
-  title = 'app';
+  name = 'Julie';   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// FormsModule does not come loaded when creating a new project using the CLI. It must be manually added
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+   declarations: [
+      AppComponent
+   ],
+   imports: [ 
+      BrowserModule, 
+      FormsModule
+   ],
   providers: [],
   bootstrap: [AppComponent]
 })
