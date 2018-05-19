d9970e3b0c2f1e0fd44b8e18ab1974cd52340701
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2018-05-19 02:03:17 -0500

    added data event binding for the server name

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,16 +9,37 @@ export class ServersComponent implements OnInit {
    // adding new property to the class and related to the 'Add Server' button.
    // when set to false, the button is not functional and the user won't be able to add a server.  
    allowNewServer = false;
-   
+   serverCreationStatus = 'No server was created';
+   // serverName starts out as an empty string, but will take in whatever is entered in the input field.
+   serverName = '';
+
+
    // The constructor is a method that is executed at the time that Angular creates the component.
    constructor() { 
       // after 2 seconds, setTimeout un-disables the 'Add Server' button.
       setTimeout( () => {
          this.allowNewServer = true;
-      } ,2000);
+      }, 2000);
    }
 
    ngOnInit() {
-  }
+   }
+
+   // method called from the DOM when 'Add Server' button is clicked. When the method is called, the serverStatusCreation property value is changed to 'Server was created'
+   onCreateServer(){
+     this.serverCreationStatus = 'Server was created';
+   }
+
+   onUpdateServerName(event: any) {
+      console.log(event);
+      // event refers to the input event of entering a server name in the input field. 
+      // target is the input element.
+      // value is what is entered in the input element as the server name.
+      // may need to use:
+      // this.serverName = (<HTMLInputElement>event.target).value 
+      //  if the code editor does not like it without.
+      // this is called 'casting'
+      this.serverName = event.target.value;
+   }
 
 }
